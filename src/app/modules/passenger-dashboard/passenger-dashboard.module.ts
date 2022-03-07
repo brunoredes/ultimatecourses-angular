1eac8e5b57b77543da9c9439c4273b626128332c
Bruno Donatelli <bruno.donatelli@etec.sp.gov.br>
2022-03-07 20:36:12 -0300

    learning how to use httpclient

--- src/app/modules/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/modules/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,22 +1,19 @@
 import { PassengerDashboardService } from './passenger-dashboard.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { PassengerDashboardComponent } from './container/passenger-dashboard/passenger-dashboard.component';
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
-
-
 
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
     PassengerCountComponent,
-    PassengerDetailComponent
+    PassengerDetailComponent,
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, HttpClientModule],
   exports: [PassengerDashboardComponent],
-  providers: [PassengerDashboardService]
+  providers: [PassengerDashboardService],
 })
-export class PassengerDashboardModule { }
+export class PassengerDashboardModule {}
