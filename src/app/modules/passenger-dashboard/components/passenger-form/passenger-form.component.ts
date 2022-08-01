b7d335f5a03be78866dfa2c8ee32315b9f4b3d33
Just Friends <brino@pop-os.localdomain>
2022-08-01 16:54:04 -0300

    feat: added a static array to iterate a select with many options

--- src/app/modules/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/modules/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Passenger } from '../../models';
+import { Passenger, Baggage } from '../../models';
 
 @Component({
   selector: 'app-passenger-form',
@@ -8,12 +8,31 @@ import { Passenger } from '../../models';
 })
 export class PassengerFormComponent implements OnInit {
   @Input() public detail: Passenger;
+  public baggage: Baggage[] = [
+    {
+      key: 'none',
+      value: 'No baggage',
+    },
+    {
+      key: 'hand-only',
+      value: 'Hand baggage',
+    },
+    {
+      key: 'hold-only',
+      value: 'Hold baggage',
+    },
+    {
+      key: 'hand-hold',
+      value: 'Hand and hold baggage',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
 
   public toggleCheckIn(checkedIn: boolean) {
-    if(checkedIn) {
+    if (checkedIn) {
       this.detail.checkInDate = Date.now();
     }
   }
